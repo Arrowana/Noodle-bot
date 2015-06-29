6f6e23066b057f6c050c071b5ab8ed45b206887e
ctrlworks <pierre.duval@gadz.org>
2015-06-29 15:46:07 +0800

    add drift.py to compute drift on a period of time

---
+++ drift.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+import rospy
+import serial
+import random
+from threading import Thread, Event
+import time
+import math as m
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+import signal
+import sys
+
+SERIALPORT="/dev/ttyUSB0"
+
+class DataContainer(Thread):
+	def __init__(self, line):
+		Thread.__init__(self)
+		
+		self.line=line
+
+		self.ser = serial.Serial(SERIALPORT, 115200)
+		self.data_dict={}
+
+		self.calibrated=False
+
+		self.s_acc=1
+		self.s_gyro=4*131*180/m.pi # sensivity+ deg to rad
+		self.N_sample=200
+		self.alpha=0.98
+
+		self.start_time=rospy.Time.now()
+
+		self.recorded_data=["GyX","GyY","GyZ"]
+
+	def run(self):   
+		for i in range(1000):
+			#print("Data received")
+			data_raw=self.ser.readline()
+			#print(data_raw)
+			data=data_raw.decode("utf-8")
+			self.process(str(data))
+
+	def process(self, data):
+		if "data:" in data:
+			try:
+				header, data_name=data.split(":")
+				data_name,=data_name.splitlines()
+				# Read value associated to data_name
+				value_raw=self.ser.readline()
+				value=float(value_raw.decode("utf-8"))
+			except:
+				print("Unexpected format of data")
+				return
+
+			if data_name in self.recorded_data:
+				# Store in dictionnary and add key if doesn't exist
+				if data_name in self.data_dict:
+					self.data_dict[data_name].append(value)
+				else:
+					self.data_dict[data_name]=[value]
+
+			if "GyX" in self.data_dict:
+				self.compute()
+
+	def update(self, frameNum):
+		data_name=self.line.get_label()
+		if data_name in self.data_dict:
+			self.modify(self.line, data_name)
+
+	def modify(self, line, data_name):
+		length=len(self.data_dict[data_name])
+		line.set_data(range(length), self.data_dict[data_name])
+		line.axes.set_xlim(0, length)
+
+		y_max=max([abs(x) for x in self.data_dict[data_name]])
+		line.axes.set_ylim(-y_max, y_max)
+
+		print(self.data_dict[data_name])
+			
+	def compute(self):
+		if self.calibrated==False and len(self.data_dict["GyX"])>self.N_sample+1:
+			self.calibrate()
+
+			self.data_dict["gyro_x"]=[]
+			self.data_dict["gyro_y"]=[]
+			self.data_dict["gyro_z"]=[]
+
+			self.data_dict["gyro_roll"]=[0]
+			self.data_dict["gyro_pitch"]=[0]
+			self.data_dict["gyro_yaw"]=[0]
+
+		if self.calibrated==True:
+			dt=0.02
+			
+			gyro_x=(self.data_dict["GyX"][-1]-self.gyro_x_cal)/self.s_gyro
+			self.data_dict["gyro_x"].append(gyro_x)
+			gyro_y=(self.data_dict["GyY"][-1]-self.gyro_y_cal)/self.s_gyro
+			self.data_dict["gyro_y"].append(gyro_y)
+			gyro_z=(self.data_dict["GyZ"][-1]-self.gyro_z_cal)/self.s_gyro
+			self.data_dict["gyro_z"].append(gyro_z)
+
+			# Roll, pitch and yaw from gyroscope
+			gyro_roll=self.data_dict["gyro_roll"][-1]+180.*dt*gyro_x/m.pi
+			self.data_dict["gyro_roll"].append(gyro_roll)
+			
+			gyro_pitch=self.data_dict["gyro_pitch"][-1]+180.*dt*gyro_y/m.pi
+			self.data_dict["gyro_pitch"].append(gyro_pitch)
+
+			gyro_yaw=self.data_dict["gyro_yaw"][-1]+180.*dt*gyro_z/m.pi
+			self.data_dict["gyro_yaw"].append(gyro_yaw)
+
+			if (rospy.Time.now()-self.last_time).to_sec()>1.:
+				print("gyro_yaw : "+str(gyro_yaw))
+				self.last_time=rospy.Time.now()
+
+	def calibrate(self):
+		print("CALIBRATING===========================")
+		self.gyro_x_cal=sum(self.data_dict["GyX"][0:self.N_sample])/self.N_sample
+		self.gyro_y_cal=sum(self.data_dict["GyY"][0:self.N_sample])/self.N_sample
+		self.gyro_z_cal=sum(self.data_dict["GyZ"][0:self.N_sample])/self.N_sample
+
+		self.calibrated=True
+		print("CALIBRATED")
+
+	def stop(self, signal, frame):
+		sys.exit(0)
+
+if __name__ == '__main__':
+	rospy.init_node('drift', anonymous=True)
+	print("Node initialized")
+
+	fig = plt.figure(0)
+	ax1=plt.subplot(121)
+	plt.title("gyro_yaw")
+	line_yaw, = ax1.plot([],[],label="gyro_yaw")
+
+	#Create data container
+	data=DataContainer(line_yaw)
+	data.daemon=True
+
+	signal.signal(signal.SIGINT, data.stop)
+	data.start()
+
+	anim = animation.FuncAnimation(fig, data.update, interval=50)
+	plt.show()
+   
+
+
--- plot_acc.py
+++ plot_acc.py
@@ -29,7 +29,7 @@ class DataContainer(Thread):
 
         self.s_acc=1
         self.s_gyro=4*131*180/m.pi # sensivity+ deg to rad
-        self.N_sample=100
+        self.N_sample=200
         self.alpha=0.98 #tau/(tau+dt)
 
         self.imu_raw_pub = rospy.Publisher('imu_raw', Imu, queue_size=10)
@@ -55,7 +55,7 @@ class DataContainer(Thread):
                 self.modify(line, data_name)
 
     def run(self):   
-        for i in range(100000):
+        while True:
             #print("Data received")
             data_raw=self.ser.readline()
             #print(data_raw)
@@ -71,8 +71,6 @@ class DataContainer(Thread):
                 data_name,=data_name.splitlines()
                 # Read value associated to data_name
                 value_raw=self.ser.readline()
-                print(data_name)
-                print(value_raw)
                 value=float(value_raw.decode("utf-8"))
             except:
                 print("Unexpected format of data")
@@ -111,6 +109,14 @@ class DataContainer(Thread):
         if self.calibrated==False and len(self.data_dict["AcX"])>self.N_sample+1:
             self.calibrate()
 
+            self.data_dict["acc_x"]=[]
+            self.data_dict["acc_y"]=[]
+            self.data_dict["acc_z"]=[]
+
+            self.data_dict["gyro_x"]=[]
+            self.data_dict["gyro_y"]=[]
+            self.data_dict["gyro_z"]=[]
+
             self.data_dict["acc_pitch"]=[]
             self.data_dict["acc_roll"]=[]
 
@@ -123,41 +129,44 @@ class DataContainer(Thread):
 
         if self.calibrated==True:
             dt=0.02
-            
+
             acc_x=(self.data_dict["AcX"][-1]-self.acc_x_cal)/self.s_acc
+            self.data_dict["acc_x"].append(acc_x)
             acc_y=(self.data_dict["AcY"][-1]-self.acc_y_cal)/self.s_acc
+            self.data_dict["acc_y"].append(acc_y)
             acc_z=(self.data_dict["AcZ"][-1]-self.acc_z_cal)/self.s_acc
+            self.data_dict["acc_z"].append(acc_z)
             
             gyro_x=(self.data_dict["GyX"][-1]-self.gyro_x_cal)/self.s_gyro
+            self.data_dict["gyro_x"].append(gyro_x)
             gyro_y=(self.data_dict["GyY"][-1]-self.gyro_y_cal)/self.s_gyro
+            self.data_dict["gyro_y"].append(gyro_y)
             gyro_z=(self.data_dict["GyZ"][-1]-self.gyro_z_cal)/self.s_gyro
-            
+            self.data_dict["gyro_z"].append(gyro_z)
 
             # Roll and pitch from accelerometer
-            acc_roll=m.atan2(acc_y, acc_z)
+            acc_roll=180.*m.atan2(acc_y, acc_z)/m.pi
             self.data_dict["acc_roll"].append(acc_roll)
 
             acc_pitch=-180.*m.atan2(acc_x,acc_z)/m.pi
             self.data_dict["acc_pitch"].append(acc_pitch)
-            print("acc_pitch : "+str(acc_pitch))
+            #print("acc_pitch : "+str(acc_pitch))
 
             # Roll, pitch and yaw from gyroscope
-            gyro_roll=self.data_dict["gyro_roll"][-1]+dt*gyro_x
+            gyro_roll=self.data_dict["gyro_roll"][-1]+180.*dt*gyro_x/m.pi
             self.data_dict["gyro_roll"].append(gyro_roll)
             
             gyro_pitch=self.data_dict["gyro_pitch"][-1]+180.*dt*gyro_y/m.pi
             self.data_dict["gyro_pitch"].append(gyro_pitch)
 
-            K=1.
-            gyro_yaw=K*(self.data_dict["gyro_yaw"][-1]/K+dt*gyro_z)
+            gyro_yaw=self.data_dict["gyro_yaw"][-1]+180.*dt*gyro_z/m.pi
             self.data_dict["gyro_yaw"].append(gyro_yaw)
 
             # Fusion and filtering of data
-            #
             forceMagnitude=abs(acc_x)+abs(acc_y)+abs(acc_z)
 
             last_roll=self.data_dict["roll"][-1]
-            roll=self.alpha*(last_roll-gyro_x*dt)+(1-self.alpha)*acc_roll
+            roll=self.alpha*(last_roll+180.*gyro_x*dt/m.pi)+(1-self.alpha)*acc_roll
             self.data_dict["roll"].append(roll)
 
             last_pitch=self.data_dict["pitch"][-1]
@@ -165,22 +174,21 @@ class DataContainer(Thread):
             #pitch=(self.alpha*(last_pitch+gyro_y*dt)+(1-self.alpha)*acc_pitch)
             self.data_dict["pitch"].append(pitch)
 
-            sample_size=2000
+            sample_size=100
             if(len(self.data_dict["GyX"])==sample_size):
                 drift=abs(self.data_dict["GyX"][-1]-self.data_dict["GyX"][0])/(1000*dt)
                 print("drift : "+str(drift))
-            elif(len(self.data_dict["GyX"])<sample_size):
-                print("GyX : "+str(self.data_dict["GyX"][-1]))
-                print("gyro_pitch : "+ str(gyro_y))
+            #elif(len(self.data_dict["GyX"])<sample_size):
+            #print("GyX : "+str(self.data_dict["GyX"][-1]))
+            #print("gyro_pitch : "+ str(gyro_y))
 
             #self.publish_ros()
-
             #self.clean_data_dict()
 
     def clean_data_dict(self):
         for key in self.data_dict.keys():
             length=len(self.data_dict[key])
-            if(length>300):
+            if(length>400):
                 del self.data_dict[key][150:]
 
             print(length)
@@ -196,6 +204,7 @@ class DataContainer(Thread):
         self.gyro_z_cal=sum(self.data_dict["GyZ"][0:self.N_sample])/self.N_sample
 
         self.calibrated=True
+        print("CALIBRATED")
 
     def pop_data(self, data_list):
         data_list.append(random.randint(0,100))
@@ -217,60 +226,53 @@ if __name__ == '__main__':
         plt.show()
 
     rospy.init_node('IMU_publisher', anonymous=True)
-        
-    x_max=300
-    y_max=8000
 
     # Set up animation
     # please keep in mind that the label of a line is used for ploting data
     fig = plt.figure(0)
     ax1=plt.subplot(331)
-    plt.title("Acc")
-    r1=[[0, x_max], [-y_max, y_max]]
-    accX, = ax1.plot([],[],label="AcX")
-    accY, = ax1.plot([],[],label="AcY")
-    accZ, = ax1.plot([],[],label="AcZ")
+    plt.title("acc calibrated")
+    acc_x, = ax1.plot([],[],label="acc_x")
+    acc_y, = ax1.plot([],[],label="acc_y")
+    acc_z, = ax1.plot([],[],label="acc_z")
     
     ax2=plt.subplot(332)
-    plt.title("Gy")
-    r2=[[0, x_max], [-200, 200]]
-    gyroX, = ax2.plot([],[], label="GyX")
-    gyroY, = ax2.plot([],[], label="GyY")
-    gyroZ, = ax2.plot([],[], label="GyZ")
+    plt.title("gyro calibrated")
+    gyro_x, = ax2.plot([],[], label="gyro_x")
+    gyro_y, = ax2.plot([],[], label="gyro_y")
+    gyro_z, = ax2.plot([],[], label="gyro_z")
 
     ax3=plt.subplot(333)
-    plt.title("acc_roll and gyro_roll")
+    plt.title("acc_roll, gyro_roll and roll")
     acc_roll, = ax3.plot([],[], label="acc_roll")
     gyro_roll, = ax3.plot([],[], label="gyro_roll")
+    roll, = ax3.plot([],[], label="roll")
+    roll.axes.set_ylim(-50, 50)
 
-    ax4=plt.subplot(334)
-    plt.title("roll")
-    roll, = ax4.plot([],[], label="roll")
-
-    ax5=plt.subplot(335)
+    ax5=plt.subplot(334)
     plt.title("acc_pitch, gyro_pitch and pitch")
     acc_pitch, = ax5.plot([],[], label="acc_pitch")
     gyro_pitch, = ax5.plot([],[], label="gyro_pitch")
     pitch, = ax5.plot([],[], label="pitch")
-    pitch.axes.set_ylim(-90, 90)
+    pitch.axes.set_ylim(-50, 50)
 
-    ax6=plt.subplot(336)
+    ax6=plt.subplot(335)
     plt.title("yaw_dmp")
     yaw_dmp, = ax6.plot([],[], label="yaw_dmp")
 
-    ax7=plt.subplot(337)
+    ax7=plt.subplot(336)
     plt.title("pitch_dmp")
     pitch_dmp, = ax7.plot([],[], label="pitch_dmp")
 
-    ax8=plt.subplot(338)
+    ax8=plt.subplot(337)
     plt.title("roll_dmp")
     roll_dmp, = ax8.plot([],[], label="roll_dmp")
 
-    ax9=plt.subplot(339)
+    ax9=plt.subplot(338)
     plt.title("gyro_yaw")
     gyro_yaw, = ax9.plot([],[], label="gyro_yaw")
     
-    lines=[accX, accY, accZ, gyroX, gyroY, gyroZ, acc_pitch, acc_roll, gyro_roll,
+    lines=[acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, acc_pitch, acc_roll, gyro_roll,
      gyro_pitch, gyro_yaw, roll, pitch, yaw_dmp, pitch_dmp, roll_dmp]
 
     #Create data container
@@ -281,7 +283,7 @@ if __name__ == '__main__':
     except(KeyboardInterrupt, SystemExit):
         print('\n! Received keyboard interrupt, quitting threads.\n')
 
-    anim = animation.FuncAnimation(fig, data.update, interval=100)
+    anim = animation.FuncAnimation(fig, data.update, interval=50)
     plt.show()
 
    
